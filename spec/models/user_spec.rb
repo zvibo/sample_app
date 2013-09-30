e4092ffac9014d70114d4613fdec90a5f69cfac6
Zvi Boshernitzan <zvi@kiva.org>
2013-09-30 00:13:10 -0700

    User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,7 +54,7 @@ describe User do
   
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com zvi@boogie..net]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -84,8 +84,7 @@ describe User do
   
   describe "when password is not present" do
      before do
-       @user = User.new(name: "Example User", email: "user@example.com",
-                        password: " ", password_confirmation: " ")
+       @user = User.new(name: "Example User", email: "user@example.com",password: " ", password_confirmation: " ")
      end
      it { should_not be_valid }
    end
@@ -95,6 +94,13 @@ describe User do
     it { should_not be_valid }
   end
   
-  
-  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { 'IamCandyJane@theidea.nET' }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
 end
